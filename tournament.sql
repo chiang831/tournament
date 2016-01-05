8cb19f6a4ff106a586149cbc21c635f047e799e8
CY Chiang <chiang831@gmail.com>
2016-01-06 01:00:38 +0800

    Create database and players table

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,14 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-
+-- \q: disconnect from database.
+-- \i tournament.sql: to run commands in this file.
+-- \dt : list table in this database.
+-- \d+ table_name: describe table.
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\connect tournament;
+CREATE TABLE players (
+    id    serial,
+    name  text
+  );
