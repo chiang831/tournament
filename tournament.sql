fee02ca7e90d1302abeb6369d4db7979a4a35636
CY Chiang <chiang831@gmail.com>
2016-01-08 01:56:57 +0800

    Add view to query standings
    Join players and matches to get two views win_records and loss_records.
    Player with no match will get 0 win_number and 0 loss_number as the view
    as we use COUNT(winner) and COUNT(loser) and LEFT JOIN.
    
    Then, join win_records and loss_records into records using player id as
    condition.
    
    Then, add win_number and loss_number in records to get played_matches.
    Join the added result with players table using player id as condition,
    and sort the result by win_number and played_matches to get the final
    standings view.

--- tournament.sql
+++ tournament.sql
@@ -21,3 +21,30 @@ CREATE TABLE matches (
     winner int references players(id),
     loser int references players(id)
   );
+
+CREATE VIEW win_records (player, win_number) AS
+  SELECT id,  COUNT(winner) AS win_number FROM players LEFT JOIN matches
+    ON id = winner GROUP BY id ORDER BY win_number;
+
+CREATE VIEW loss_records (player, loss_number) AS
+  SELECT id, COUNT(loser) AS loss_number FROM players LEFT JOIN matches 
+    ON id = loser GROUP BY id ORDER BY loss_number;
+
+CREATE VIEW records (player, win_number, loss_number) AS
+  SELECT win_records.player, win_number, loss_number FROM 
+    win_records JOIN loss_records on win_records.player = loss_records.player;
+
+CREATE VIEW standings (player, name, win_number, played_matches) AS
+  SELECT player, name, win_number, win_number + loss_number as played_matches
+    FROM players JOIN records on players.id = records.player
+      ORDER BY win_number DESC,  played_matches;
+
+-- DEBUG USAGE
+-- INSERT INTO players (name) VALUES ('A');
+-- INSERT INTO players (name) VALUES ('B');
+-- INSERT INTO players (name) VALUES ('C');
+-- INSERT INTO matches (winner, loser) VALUES (1, 2);
+-- select * from win_records;
+-- select * from loss_records;
+-- select * from records;
+-- select * from standings;
