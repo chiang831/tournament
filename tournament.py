bd1f7077155897bf3b7c3eb03ee8921e64cdb47c
CY Chiang <chiang831@gmail.com>
2016-01-14 00:28:29 +0800

    Use context manager for connection and cursor
    The context manager makes sure connection and cursor are closed.

--- tournament.py
+++ tournament.py
@@ -3,6 +3,7 @@
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
+import contextlib
 import psycopg2
 
 
@@ -11,36 +12,44 @@ def connect():
     return psycopg2.connect("dbname=tournament")
 
 
-def deleteMatches():
+@contextlib.contextmanager
+def _connect_db():
+    """Connect to DB and get cursor. Close cursor and connection on exit.
+
+    Returns:
+      A tuple (connection, cursor) connecting to tournament database.
+    """
+    try:
+        conn = connect()
+        cur = conn.cursor()        
+        yield conn, cur
+
+    finally:
+        cur.close()
+        conn.close()
+
+
+def deleteMatches(cur=None, conn=None):
     """Remove all the match records from the database."""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""DELETE FROM matches;""")
-    cur.execute("""DELETE FROM byes;""")
-    conn.commit()
-    cur.close()
-    conn.close()
+    with _connect_db() as (conn, cur):
+        cur.execute("""DELETE FROM matches;""")
+        cur.execute("""DELETE FROM byes;""")
+        conn.commit()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""DELETE FROM players;""")
-    conn.commit()
-    cur.close()
-    conn.close()
+    with _connect_db() as (conn, cur):
+        cur.execute("""DELETE FROM players;""")
+        conn.commit()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""SELECT COUNT(*) from players;""")
-    ret = int(cur.fetchone()[0])
-    cur.close()
-    conn.close()
-    return ret
+    with _connect_db() as (conn, cur):
+        cur.execute("""SELECT COUNT(*) from players;""")
+        ret = int(cur.fetchone()[0])
+        return ret
 
 
 def registerPlayer(name):
@@ -52,12 +61,9 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""INSERT INTO players (name) VALUES (%s);""", (name,))
-    conn.commit()
-    cur.close()
-    conn.close()
+    with _connect_db() as (conn, cur):
+        cur.execute("""INSERT INTO players (name) VALUES (%s);""", (name,))
+        conn.commit()
 
 
 def playerStandings():
@@ -73,13 +79,10 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""SELECT * FROM standings;""")
-    standings = cur.fetchall()
-    cur.close()
-    conn.close()
-    return standings
+    with _connect_db() as (conn, cur):
+        cur.execute("""SELECT * FROM standings;""")
+        standings = cur.fetchall()
+        return standings
 
 
 def reportMatch(winner, loser):
@@ -89,12 +92,9 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("""INSERT INTO matches VALUES (%s, %s);""", (winner, loser))
-    conn.commit()
-    cur.close()
-    conn.close()
+    with _connect_db() as (conn, cur):
+        cur.execute("""INSERT INTO matches VALUES (%s, %s);""", (winner, loser))
+        conn.commit()
  
  
 def _getAndSetByePlayer():
@@ -106,18 +106,13 @@ def _getAndSetByePlayer():
     Returns:
       An int for id number of the player who should get bye this round.
     """
-    try:
-        conn = connect()
-        cur = conn.cursor()
+    with _connect_db() as (conn, cur):
         cur.execute("""SELECT player FROM bye_candidate;""")
         query_result = cur.fetchall()
         bye_player = query_result[0]
         cur.execute("""INSERT INTO byes VALUES (%s, %s);""", (bye_player, 1))
         conn.commit()
         return bye_player
-    finally:
-        cur.close()
-        conn.close()
 
 
 def swissPairings():
@@ -135,30 +130,31 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    try:
+    with _connect_db() as (conn, cur):
         number_of_players = countPlayers()
-        conn = connect()
-        cur = conn.cursor()
         pairings = [] 
 
         if number_of_players & 1:
           # Decides bye player and skip that player in the query of standings.
           bye_id = _getAndSetByePlayer()
           for pair_index in xrange(0, number_of_players - 1, 2):
-              cur.execute("""SELECT player, name FROM standings WHERE player != %s OFFSET %s LIMIT 2;""", (bye_id, pair_index,))
+              cur.execute(
+                     """SELECT player, name FROM standings WHERE
+                             player != %s OFFSET %s LIMIT 2;""",
+                     (bye_id, pair_index,))
               query_result = cur.fetchall()
               id1, name1 = query_result[0]
               id2, name2 = query_result[1]
               pairings.append((id1, name1, id2, name2))
         else:
           for pair_index in xrange(0, number_of_players, 2):
-              cur.execute("""SELECT player, name FROM standings OFFSET %s LIMIT 2;""", (pair_index,))
+              cur.execute(
+                      """SELECT player, name FROM standings
+                              OFFSET %s LIMIT 2;""",
+                      (pair_index,))
               query_result = cur.fetchall()
               id1, name1 = query_result[0]
               id2, name2 = query_result[1]
               pairings.append((id1, name1, id2, name2))
 
         return pairings
-    finally:
-        cur.close()
-        conn.close()
